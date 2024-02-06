a30396877d6714cc881aebae3cd85df932123509
Aftermath001 <alvinasiachi@gmail.com>
2024-02-06 21:07:30 +0300

    Deleting a user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 // update user
 router.put("/:id", async(req,res)=>{
-    if(req.body.userId == req.params.id || req.user.isAdmin ){
+    if(req.body.userId == req.params.id || req.body.isAdmin ){
         if (req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -24,10 +24,24 @@ router.put("/:id", async(req,res)=>{
         }
 
     } else{
-        return res.status(403).jtson("You can only upfate your accoun")
+        return res.status(403).jtson("You can only update your account")
     }
-})
+});
 // delete user
+router.delete("/:id", async(req,res)=>{
+    if(req.body.userId == req.params.id || req.body.isAdmin ){
+       
+        try{
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted");
+        }catch(err){
+            return res.status(500).json(err);
+        }
+
+    } else{
+        return res.status(403).jtson("You can delete only your account")
+    }
+});
 // get a user
 // follow a user
 // unfollow a user 
