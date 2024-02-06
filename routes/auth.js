8b1bff58b61acc0e1d3f418c304e921a965ed54e
Aftermath001 <alvinasiachi@gmail.com>
2024-02-06 12:24:36 +0300

    (Update user information)

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,46 @@
 const router = require("express").Router();
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
-router.get("/",(req,res)=>{
-    res.send("hey its auth routes")
-})
-module.exports = router
+
+// REGISTER
+
+router.post("/register", async (req,res)=>{
+    try{
+        // Generate new password (For security)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // Create new user
+        const newUser =  new User ({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        // Save user and return response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN
+router.post("/login", async(req,res) =>{
+   try {
+    //  Search for user in the database if user email is wrong
+    const user = await User.findOne({ email:req.body.email });
+    !user && res.status(404).json("user not found");
+
+    // Comparing input password and saved password(if password is wrong)
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json("wrong credentials");
+
+    // If credentials are right
+    res.status(200).json(user);
+   } catch(err){
+    res.status(500).json(err);
+   }
+});
+module.exports = router;
